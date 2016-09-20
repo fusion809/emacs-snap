f74c56a0d2f29677d45a861d8d4270cf96fe8ddd
fusion809 <brentonhorne77@gmail.com>
2016-09-21 06:46:09 +1000

    Initial commit

--- emacs-tealeg.sh
+++ emacs-tealeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export EMACSDATA=$SNAP/share/emacs/24.5/etc
-export EMACSLOADPATH=$SNAP/share/emacs/site-lisp:$SNAP/share/emacs/24.5/lisp/
+export EMACSDATA=$SNAP/share/emacs/25.1/etc
+export EMACSLOADPATH=$SNAP/share/emacs/site-lisp:$SNAP/share/emacs/25.1/lisp/
 export INFOPATH=$SNAP/share/info/
 desktop-launch emacs "$@"
